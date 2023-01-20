4336ca1536e72b1ec572f3321f7caa5ca09b3046
Mihail Marian <mihailmarian12@gmail.com>
2023-01-20 16:57:55 +0000

    added authentication

--- main.go
+++ main.go
@@ -15,13 +15,15 @@ func main() {
 	}
 
 	openaiApiKey := os.Getenv("OPENAI_API_KEY")
-	fmt.Println(openaiApiKey)
 
 	client := &http.Client{}
 
 	url := "https://api.openai.com/v1/completions"
 
 	req, _ := http.NewRequest("GET", url, nil)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", openaiApiKey))
+
 	res, _ := client.Do(req)
 	fmt.Println(res)
 }
